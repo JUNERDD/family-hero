fb2bc882423e7ada00d766b011c4bee969d11f1a
JUNER <53842108@qq.com>
2024-07-31 18:32:21 +0800

    🔨 fix(svg): 修复横线命名

--- src/assets/svg/vector.tsx
+++ src/assets/svg/vector.tsx
@@ -16,7 +16,7 @@ const Vector: React.FC<IProps> = (props) => {
       <path
         d="M1 0.5H2.83984C3.58426 0.5 4.26708 0.913455 4.61209 1.57311L6.10462 4.42689C6.44962 5.08655 7.13245 5.5 7.87687 5.5H9.57143M6.23002 0.5H9.57143"
         stroke="currentColor"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
     </svg>
   )
